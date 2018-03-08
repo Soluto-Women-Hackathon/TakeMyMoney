a70347def7848c20e59db83be92f6784a00adecc
nogalavi <noga@indeni.com>
2018-03-08 19:36:32 +0200

    login and wheel components

---
+++ src/wheel.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Wheel extends Component {
+    render() {
+        return (
+            <div className="App">
+                wheel
+            </div>
+        );
+    }
+}
