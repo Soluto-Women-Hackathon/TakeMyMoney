ae4c4054ac59eb785e69c2d99d334c4829471ff1
nogalavi <noga@indeni.com>
2018-03-08 21:20:16 +0200

    donation button

--- src/login.js
+++ src/login.js
@@ -4,7 +4,27 @@ export default class Login extends Component {
     render() {
         return (
             <div className="App">
-                login
+
+                <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">
+
+                    <input type="hidden" name="business"
+                           value="takemymoneytosavetheworld@gmail.com"/>
+
+                        <input type="hidden" name="cmd" value="_donations"/>
+
+                            <input type="hidden" name="item_name" value="Take my money"/>
+                                <input type="hidden" name="item_number" value="One dollar to save the world"/>
+                                    <input type="hidden" name="amount" value="1.00"/>
+                                        <input type="hidden" name="currency_code" value="USD"/>
+
+                                            <input type="image" name="submit"
+                                                   src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif"
+                                                   alt="Donate"/>
+                                                <img alt="" width="1" height="1"
+                                                     src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" />
+                </form>
+
+
             </div>
         );
     }
