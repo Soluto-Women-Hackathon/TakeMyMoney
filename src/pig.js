ff026a5147509e5bb86ba4d288529e2d3665b0d3
nogalavi <noga@indeni.com>
2018-03-09 04:09:44 +0200

    piggy

---
+++ src/pig.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class Pig extends Component {
+    constructor() {
+        super();
+        this.state = {
+            roll : window.location.href.includes("#!roll")
+        }
+    }
+
+    componentWillMount() {
+        let t = setTimeout(() => this.setState({moveCoin: true}), 200)
+    }
+
+    render() {
+        return (
+        <div className="pig">
+            <img className="ear" src="https://i.imgur.com/7q5Z9DS.png"/>
+            <img className="body" src="https://i.imgur.com/r5LDd3i.png"/>
+            <img className={`coin ${this.state.moveCoin ? "move" : ""}`} src="https://i.imgur.com/bOiT8Ju.png"/>
+        </div>
+        );
+    }
+}
